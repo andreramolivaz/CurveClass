0216e23490d4c3ed6157be0dd26e05b44cf51c6e
andreramolivaz <a.ramolivaz@icloud.com>
2023-11-03 13:25:00 +0100

    refactoring

--- code/curve.cpp
+++ code/curve.cpp
@@ -1,4 +1,3 @@
-
 #include <functional>
 #include <iostream>
 #include <utility>
@@ -6,15 +5,12 @@
 
 using namespace std;
 
-
 using real = double;
 using unary_fun = function<real(const real&)>;
 
 #define RESOLUTION (10)
 
-
-class curve
-{
+class curve{
 
 private:
 	real a, b;
@@ -22,86 +18,54 @@ private:
 
 public:
 	curve(const real& a_, const real& b_, const unary_fun& f_) : f(f_), a(a_), b(b_) {}
-
 	real get_dx() const { return (b - a) / RESOLUTION; }
-
 	pair<real, real> interval() const { return pair<real, real>(a, b); }
-
-	curve derivative() const
-	{
+	curve derivative() const{
 		return curve(a, b, [&, dx = get_dx()](const real& x) {	// anche la capture [=] sarebbe stata sufficiente
 			const real dy = f(x + dx) - f(x);
 			return dy / dx;
 			});
 	}
-
-	curve primitive() const
-	{
+	curve primitive() const{
 		return curve(a, b, [&, dx = get_dx()](const real& x) {	// oppure la [&]
 			const real y = f(x);
 			return y * dx;
 			});
 	}
-
-	real integral() const
-	{
+	real integral() const{
 		const unary_fun& F = primitive();
 		return F(b) - F(a);
 	}
-
-	real operator()(const real& x) const
-	{
-		return f(x);
-	}
-
-	class iterator
-	{
+	real operator()(const real& x) const{ return f(x);}
+	class iterator{
 	private:
 		const curve& c;
 		real x;
 
 	public:
 		iterator(const curve& c_, const real& x_) : c(c_), x(x_) {}
-
-		pair<real, real> operator*() const
-		{
+		pair<real, real> operator*() const{
 			return pair<real, real>(x, c.f(x));
 		}
-
-		iterator& operator++()	// il pre-incremento modifica sè stesso e non ritorna una copia
-		{
+		// il pre-incremento modifica sÃ¨ stesso e non ritorna una copia
+		iterator& operator++(){
 			x += c.get_dx();
 			return *this;
 		}
-
-		iterator operator++(int)	// il post-incremento fa una copia, modifica sé stesso e ritorna la copia
-		{
+		// il post-incremento fa una copia, modifica sÃ© stesso e ritorna la copia
+		iterator operator++(int){
 			auto r(*this);
 			++(*this);
 			return r;
 		}
-
-		bool operator!=(const iterator& it) const
-		{
-			return fabs(x - it.x) >= c.get_dx();	// non si confrontano mai i float direttamente con l'operatore di uguaglianza o disuguaglianza
+		bool operator!=(const iterator& it) const{
+			return fabs(x - it.x) >= c.get_dx();
 		}
 	};
-
-	iterator begin() const
-	{
-		return iterator(*this, a);
-	}
-
-	iterator end() const
-	{
-		return iterator(*this, b + get_dx());
-	}
-
+	iterator begin() const{ return iterator(*this, a);}
+	iterator end() const{ return iterator(*this, b + get_dx());}
 };
-
-
-ostream& operator<<(ostream& os, const curve& c)
-{
+ostream& operator<<(ostream& os, const curve& c){
 	os << "dom = [" << c.interval().first << ", " << c.interval().second << "] dx = " << c.get_dx() << ": " << endl;
 	/*for (const pair<real, real>& p : c)
 	{
@@ -117,10 +81,8 @@ ostream& operator<<(ostream& os, const curve& c)
 	return os << endl;
 }
 
-int main()
-{
+int main(){
 	curve f(-1., 1., [](const real& x) { return x * x; });
-
 	cout << f << endl
 		<< f.derivative() << endl
 		<< f.primitive() << endl
